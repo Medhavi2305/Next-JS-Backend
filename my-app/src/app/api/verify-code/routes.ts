7de786b4cc0e677fa62b2b01680994776cc32bdc
Medhavi <medhavisharma2305@gmail.com>
2026-07-10 23:48:44 +0000

    Verify Code

--- my-app/src/app/api/verify-code/routes.ts
+++ my-app/src/app/api/verify-code/routes.ts
@@ -3,22 +3,44 @@ import UserModel from "@/model/user.model";
 
 export async function POST(request: Request){
     await dbConnect()
+
     try {
         const {username, code} = await request.json()
         const decodedUsername = decodeURIComponent(username)
-        const user = UserModel.findOne({username: decodedUsername})
+        const user = await UserModel.findOne({username: decodedUsername})
         if(!user){
-           return Response.json({
-            success: false,
-            message: 'User not found'
-        }, {status: 500}) 
+            return Response.json({
+                success: false,
+                message: 'user not found'
+            }, {status: 500})
         }
-        
+        const isCodeValid = user.verifyCode === code
+        const isCodeNotExpierd = new Date(user.verifyCodeExpiry) > new Date()
+
+        if(isCodeValid && isCodeNotExpierd){
+            user.isVerifyed = true 
+            await user.save()
+
+            return Response.json({
+                success: true,
+                message: 'Account Verified Successfully'
+            }, {status: 200})
+        }else if(!isCodeNotExpierd){
+            return Response.json({
+                success: false,
+                message: 'User code expired'
+            }, {status: 500})
+        }else if(!isCodeValid){
+            return Response.json({
+                success: false,
+                message: 'Code incorrect'
+            }, {status: 500})
+        }
     } catch (error) {
-        console.error(`Error verifying user ${error}`)
+        console.log('verifing user', error)
         return Response.json({
             success: false,
-            message: 'Error verifying user'
-        }, {status: 500})
+            message: 'Error in verifing code'
+        },{status: 500})
     }
 }
