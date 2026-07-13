7fdcd799985158c28806df410100ed066dcaacd4
Medhavi <medhavisharma2305@gmail.com>
2026-07-13 22:56:50 +0000

    Ai PArt added

--- my-app/src/app/api/suggest-messages/route.ts
+++ my-app/src/app/api/suggest-messages/route.ts
@@ -1,7 +1,34 @@
 import { openai } from '@ai-sdk/openai';
 import { generateText } from 'ai';
-import { NextResponse } from 'next/server';
 
-export async function POST(request: Request){
-   
+export async function POST(){
+   try {
+      const prompt = `
+      Generate exactly three anonymous, friendly, open-ended questions.
+
+      Separate each question with ||
+
+      Return only the questions and nothing else.
+      `;
+
+      const {text} = await generateText({
+         model: openai('gpt-5'),
+         prompt
+      })
+
+      return Response.json({
+         success: true,
+         message: text
+      });
+   } catch (error) {
+    console.error("Error generating message:", error);
+
+    return Response.json(
+        {
+            success: false,
+            message: "Failed to generate message",
+        },
+        { status: 500 }
+    );
+}
 }
