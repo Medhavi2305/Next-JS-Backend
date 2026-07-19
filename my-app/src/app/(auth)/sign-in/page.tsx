5694de6906db8e710c3ecacfaa6cc614c9482efb
Medhavi <medhavisharma2305@gmail.com>
2026-07-19 23:06:20 +0000

    Sign-in

--- my-app/src/app/(auth)/sign-in/page.tsx
+++ my-app/src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import * as z from "zod";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -11,6 +11,10 @@ import { signUpSchema } from "@/schemas/signUpSchema";
 import axios, { AxiosError } from 'axios'
 import { ApiResponce } from "@/types/ApiResponse";
 import { toast } from "sonner";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 
 function page() {
   const [username, setUsername]= useState('');
@@ -73,7 +77,70 @@ function page() {
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Message App</h1>
           <p className="mb-4">Sign in to continue your secret conversations</p>
         </div>
-        
+        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller
+            name="username"
+            control={form.control}
+            render={({field, fieldState}) => (
+              <Field>
+                <FieldLabel>
+                  Username
+                </FieldLabel>
+                <Input 
+                {...field}
+                onChange={(e) => {
+                  field.onChange(e)
+                  setUsername(e.target.value)
+                }}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+            />
+            <Controller 
+            name="email"
+            control={form.control}
+            render={({field, fieldState}) => (
+              <Field>
+                <FieldLabel>Email</FieldLabel>
+                <Input 
+                {...field}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+            />
+            <Controller 
+            name="password"
+            control={form.control}
+            render={({field, fieldState}) => (
+              <Field>
+                <FieldLabel>Passowrd</FieldLabel>
+                <Input 
+                {...field}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+            />
+          </FieldGroup>
+          <Button type="submit" disabled={isSubmitting}>
+            {
+              isSubmitting ? (
+                <>
+                <Loader2 className="mr-2 h-3 w-3 animate-spin" /> Please Wait
+                </>
+              ) : ('Sign-Up')
+            }
+          </Button>
+        </form>
       </div>
     </div>
   )
