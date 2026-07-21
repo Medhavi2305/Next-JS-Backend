26ba8a1e6d4d48865c513af04b74f02279f7d33f
Medhavi <medhavisharma2305@gmail.com>
2026-07-21 18:33:37 +0000

    Sign in page

--- my-app/src/app/(auth)/sign-in/page.tsx
+++ my-app/src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,90 @@
-import React from 'react'
+'use client'
+
+import { signInSchema } from "@/schemas/signInSchema"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
+import { Controller, useForm } from "react-hook-form"
+import z from "zod"
+import { signIn } from "next-auth/react";
+import { toast } from "sonner"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 function page() {
+  const router = useRouter()
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: ''
+    }
+  })
+  const onSubmit = async(data: z.infer<typeof signInSchema>) => {
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+    if(result?.error){
+      toast.error('Incorrect username or password', {position: "top-center"})
+    }
+
+    if(result?.url){
+      router.replace('/dashboard')
+    }
+  }
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Signin to continue your secert conversations</p>
+        </div>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <Controller 
+            name="identifier"
+            control={form.control}
+            render={({field, fieldState}) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel>Email/Username</FieldLabel>
+                <Input {...field}/>
+                {fieldState.invalid && (
+                  <FieldError  errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+            />
+            <Controller 
+            name="password"
+            control={form.control}
+            render={({field, fieldState}) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel>Password</FieldLabel>
+                <Input {...field}/>
+                {fieldState.invalid && (
+                  <FieldError  errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
+            />
+          </FieldGroup>
+          <Button className='w-full' type="submit">Sign In</Button>
+        </form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
